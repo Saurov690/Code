17e713b99cff91189022dffba0d22215e5f5f808
Abul Bashar Saurov <127825591+Saurov690@users.noreply.github.com>
2025-03-01 03:04:15 +0600

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,14 @@
+name="MacBook Pro"
+company="Apple"
+year=2019
+
+print(name)
+print(company)
+print(year)
+
+#
+print("My laptop is", name, "from", company, "in", year)
+print("My laptop is",name)
+print("Company is",company)
+print("Release year is",year)
+
